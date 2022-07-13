f8370db27595a8b8005ed9f3d1c1df434483d608
Karen Siu <k3tang@gmail.com>
2022-07-13 14:45:24 -0700

    add author method

--- questions.rb
+++ questions.rb
@@ -1,6 +1,6 @@
 require 'sqlite3'
 require 'singleton'
-
+require "byebug"
 class QConnection < SQLite3::Database
     include Singleton
 
@@ -12,13 +12,13 @@ class QConnection < SQLite3::Database
             
 end
 
-class Users
+class User
 
     attr_accessor :id, :fname, :lname
 
     def self.all
-        data = QConnection.instance.execute("SELECT * FROM users")
-        data.map{|datum| Users.new(datum)} # this is an array 
+        data = QConnection.instance.execute("SELECT * FROM User")
+        data.map{|datum| User.new(datum)} # this is an array 
     end 
 
     def self.find_by_id(id)
@@ -32,9 +32,10 @@ class Users
     SQL
     return nil if id.length == 0
 
-    Users.new(id.first) #this is a hash 
+    User.new(id.first) #this is a hash 
     end 
-      def self.find_by_name(fname,lname)
+
+    def self.find_by_name(fname,lname)
         name = QConnection.instance.execute(<<-SQL, fname,lname)
         SELECT
             *
@@ -46,7 +47,7 @@ class Users
     SQL
     return nil if name.length == 0
 
-    Users.new(name.first) #this is a hash 
+    User.new(name.first) #this is a hash 
     end 
 
     def initialize(hash)
@@ -56,15 +57,12 @@ class Users
     end 
    
     def authored_questions(id)
-       question_misc = Question.find_by_user_id(id) #this is a hash
-        
-       return question_misc.select {|k,v| k == 'body'}
-        
+       question_misc = Question.find_by_user_id(id) #this is an instance
+        question_misc.body #pulls out the body instance in the Question
     end 
 
 
 end 
-
 
 
 class Question
@@ -87,9 +85,29 @@ class Question
     SQL
     return nil if user_id.length == 0
 
-    Question.new(user_id.first) #this is a hash 
-
+    Question.new(user_id.first) #user_id is [{}], .first makes it {}, Question.new makes it an instance
     end
+
+    def author
+        author = QConnection.instance.execute(<<-SQL, self.title, self.body)
+            SELECT
+                fname, lname
+            FROM 
+                users 
+            WHERE 
+                users.id = (
+                    SELECT
+                       questions.user_id
+                    FROM 
+                        questions
+                    WHERE
+                        title = ? AND body = ?)
+            SQL
+        return nil if author.length == 0
+        
+        user_instance = User.new(author.first)
+        return user_instance.fname + " " + user_instance.lname
+    end 
 
 end
 
